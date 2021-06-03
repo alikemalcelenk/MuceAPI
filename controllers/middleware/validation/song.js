e00bd7f74228ed7e9354c69ac8419502bde97ca4
Ali Kemal Çelenk <alikemalcelenkk@gmail.com>
2021-06-03 14:00:32 +0300

    create validation of song/add with joi

---
+++ controllers/middleware/validation/song.js
@@ -0,0 +1,20 @@
+const Joi = require('joi');
+
+exports.add = (req, res, next) => {
+  const addSchema = Joi.object().keys({
+    name: Joi.string().trim().max(300).required(),
+    artist: Joi.string().trim().max(300).required(),
+    emotion: Joi.string().trim().max(15).required(),
+    spotifyUrl: Joi.string().trim().max(500).required(),
+    youtubeUrl: Joi.string().trim().max(500).required(),
+  });
+
+  addSchema
+    .validateAsync(req.body)
+    .then(() => {
+      next();
+    })
+    .catch(() => {
+      res.status(500).json({ data: 'Validation Error' });
+    });
+};
--- routes/song.js
+++ routes/song.js
@@ -1,6 +1,7 @@
 const songController = require('../controllers/song');
+const songValidation = require('../controllers/middleware/validation/song');
 const router = require('express').Router();
 
-router.post('/', songController.add);
+router.post('/', songValidation.add, songController.add);
 
 module.exports = router;
