635ba54e38197231b42db7c0bbd51944b133ac80
Ali Kemal Çelenk <alikemalcelenkk@gmail.com>
2021-06-03 13:41:42 +0300

    create song model

---
+++ models/Song.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const SongSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, '`{PATH}` is necessary.'],
+  },
+  artist: {
+    type: String,
+    required: [true, '`{PATH}` is necessary.'],
+  },
+  emotion: {
+    type: String,
+    required: [true, '`{PATH}` is necessary.'],
+  },
+  spotifyUrl: {
+    type: String,
+    required: [true, '`{PATH}` is necessary.'],
+  },
+  youtubeUrl: {
+    type: String,
+    required: [true, '`{PATH}` is necessary.'],
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Song', SongSchema);
